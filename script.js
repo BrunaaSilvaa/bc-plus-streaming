87536a5f0b3c8816746cf71a4783db8d9bca099b
BrunaaSilvaa <brunasoaresilvaa@gmail.com>
2026-08-18 10:05:01 -0300

    fix: corrige links quebrados e adiciona menu mobile funcional

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector('.mobile__menu')
+
+const menuLinks = document.querySelector('.mobile__links')
+
+menuBtn.addEventListener('click', () => {
+    menuLinks.classList.toggle('active')
+} )
