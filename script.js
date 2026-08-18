2569e092eb0ff31294a728f333d4669c951cba11
BrunaaSilvaa <brunasoaresilvaa@gmail.com>
2026-08-18 11:08:28 -0300

    feat: fecha menu mobile ao clicar fora ou em um link

--- script.js
+++ script.js
@@ -2,8 +2,25 @@ const menuBtn = document.querySelector('.mobile__menu')
 
 const menuLinks = document.querySelector('.mobile__links')
 
-menuBtn.addEventListener('click', () => {
+//Abre/fecha menu ao clicar no menu hamburguer
+menuBtn.addEventListener('click', (event) => {
+    event.stopPropagation() // evita que esse clique seja capturado pelo listener do document
     menuLinks.classList.toggle('active')
 } )
 
-alert ('script carregado')
+//Fecha ao clicar em qualquer link dentro dele
+
+const menuItemLinks = document.querySelectorAll('a')
+menuItemLinks.forEach (link => {
+    link.addEventListener('click', () => {
+        menuLinks.classList.remove('active')
+    })
+})
+
+//Fecha ao clicar fora do menu
+document.addEventListener('click', (event) => {
+    const clicouDentroDoMenu = event.target.closest('.mobile__container')
+    if (!clicouDentroDoMenu && menuLinks.classList.contains('active')) {
+        menuLinks.classList.remove('active')
+    }
+})
